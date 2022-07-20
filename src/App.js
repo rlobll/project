a300b39f7180a54562a37f9a96b8c4b7f62fa15d
eunbi <dmsql3335@gmail.com>
2022-07-21 02:59:51 +0900

    입력창

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const onSubmit = () => {
+    axios.post("http://localhost:3001/posts", {
+      title,
+      body,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <>
+          <Link to="">Home</Link>
+          <Link to="blogs">Blogs</Link>
+        </>
+        <Route path="/">Home Page</Route>
+        <Route path="/blog">
+          <div className="container">
+            <div className="mb-3">
+              <label className="form-label">Title</label>
+              <input
+                className="form-control"
+                value={title}
+                onChange={(event) => {
+                  setTitle(event.target.value);
+                }}
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Body</label>
+              <textarea
+                className="form-control"
+                value={body}
+                onChange={(event) => {
+                  setBody(event.target.value);
+                }}
+                rows="20"
+              />
+            </div>
+            <button className="btn btn-primary" onClick={onSubmit}>
+              Post
+            </button>
+          </div>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
