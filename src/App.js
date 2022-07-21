9db830d6927688b2733799fcf80b02d118bffa14
eunbi <dmsql3335@gmail.com>
2022-07-22 01:15:08 +0900

    블로그추가

--- src/App.js
+++ src/App.js
@@ -1,53 +1,11 @@
-import { useState } from "react";
-import axios from "axios";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Blogs from "./component/Blogs";
 
 function App() {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-  const onSubmit = () => {
-    axios.post("http://localhost:3001/posts", {
-      title,
-      body,
-    });
-  };
-
   return (
     <BrowserRouter>
       <Routes>
-        <>
-          <Link to="">Home</Link>
-          <Link to="blogs">Blogs</Link>
-        </>
-        <Route path="/">Home Page</Route>
-        <Route path="/blog">
-          <div className="container">
-            <div className="mb-3">
-              <label className="form-label">Title</label>
-              <input
-                className="form-control"
-                value={title}
-                onChange={(event) => {
-                  setTitle(event.target.value);
-                }}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Body</label>
-              <textarea
-                className="form-control"
-                value={body}
-                onChange={(event) => {
-                  setBody(event.target.value);
-                }}
-                rows="20"
-              />
-            </div>
-            <button className="btn btn-primary" onClick={onSubmit}>
-              Post
-            </button>
-          </div>
-        </Route>
+        <Route path="/" element={<Blogs />}></Route>
       </Routes>
     </BrowserRouter>
   );
